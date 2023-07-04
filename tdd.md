07f75a2950248093306f754c0ce36beaca0dd20d
VictorMoreno <victor@goin.app>
2023-07-04 21:31:32 +0200

    review domain model

---
+++ tdd.md
@@ -0,0 +1,6 @@
+[x] starts w hunger 50
+[x] increases 10 hunger per turn
+[] decreases 15 happy per turn
+[] decreases 10 training per turn
+[] Feed increment happy by 5
+[x] Feed decreses hunger by 30
--- test/ePet.test.ts
+++ test/ePet.test.ts
@@ -5,16 +5,46 @@ describe('Pet', () => {
     const pet = new Pet();
     expect(pet.getHunger()).equal(50);
   });
+
+  it("should increase hunger", () => {
+    const pet = new Pet();
+    pet.deltaHunger(10);
+    expect(pet.getHunger()).equal(60);
+  });
+
+  it("should decrease hunger by 30 on feed", () => {
+    const pet = new Pet();
+    pet.deltaHunger(-10);
+    expect(pet.getHunger()).equal(40);
+  });
+
 });
 
 
 class Pet {
+
   constructor(
-    private readonly hunger: number = 50,
+    private hunger: number = 50,
+    private happyness: number = 50,
+    private health: number = 50,
+
   ) { }
 
   getHunger(): number {
     return this.hunger;
+  }
+
+  getHappyness(): number {
+    return this.happyness;
+  }
+
+  getHealth(): number {
+    return this.health;
+  }
+
+
+  deltaHunger(delta: number) {
+    this.hunger += delta;
   }
 
 }
